9b12f845b28b8f276ce73b4a03ef3ad6c33ba870
LucaSanjevic <luca-sanjevic@hotmail.it>
2021-05-14 18:31:09 +0200

    modificato in arrow function

--- myScript.js
+++ myScript.js
@@ -7,7 +7,7 @@ let root = new Vue({
         creaEmails: function() {
         for (var i=0; i < 10; i++) {
            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-           .then(function (response) {
+           .then((response) => {
             this.listaEmails.push(response.data.response)
                                     });
                              }
